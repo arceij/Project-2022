e7bdd65e31c51e1162efb427e531d8fe91660d70
George "Neohanja" Hall <canunja@gmail.com>
2018-10-08 14:41:12 -0400

    Enemies (Part one)
    I have added Enemies to the script, as well as controls. Tweeking them later, but They move and can bump into each other, for now.

---
+++ Cherry Dew/Assets/Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Movement controls
+    public float speed;
+    public LayerMask enemyMask;
+    private Rigidbody2D rb2D;
+    private Transform enemyTransform;
+    private float enemyWidth, enemyHeight;
+    public float forwardCheck = 0.05f;
+    
+    //Variables for the implimentation of mechanics
+    public int playerDamage;
+
+	// Use this for initialization
+	public void Start ()
+    {
+        rb2D = this.GetComponent<Rigidbody2D>();
+        enemyTransform = this.GetComponent<Transform>();
+        enemyWidth = this.GetComponent<PolygonCollider2D>().bounds.extents.x;
+        enemyHeight = this.GetComponent<PolygonCollider2D>().bounds.extents.y;
+	}
+
+    private Vector2 convertto2D(Vector3 temp)
+    {
+        return new Vector2(temp.x, temp.y);
+
+    }
+
+    public void FixedUpdate()
+    {
+        Vector2 lineCastPos = enemyTransform.position - enemyTransform.right * enemyWidth;
+        Debug.DrawLine(lineCastPos, lineCastPos + Vector2.down);
+        bool isGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down * .5f, enemyMask);
+        Debug.DrawLine(lineCastPos, lineCastPos - convertto2D(enemyTransform.right) * forwardCheck);
+        bool isBlocked = Physics2D.Linecast(lineCastPos, lineCastPos - convertto2D(enemyTransform.right) * forwardCheck, enemyMask);
+
+        //If there is no ground or enemy is blocked, turn around!
+        if(!isGrounded || isBlocked)
+        {
+            Vector3 currRot = enemyTransform.eulerAngles;
+            currRot.y += 180;
+            enemyTransform.eulerAngles = currRot;
+        }
+
+        //For now, always move forward
+        Vector2 enemyVelocity = rb2D.velocity;
+        enemyVelocity.x = -enemyTransform.right.x * speed;
+        rb2D.velocity = enemyVelocity;
+    }
+}
--- Cherry Dew/Assets/Scripts/Player.cs
+++ Cherry Dew/Assets/Scripts/Player.cs
@@ -8,146 +8,63 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //Declaration of Variables for movement
-    public float speed = 5f;
-    public float accel = 3f;
-    private Vector2 input;
+    //Declaration of Variables for movement - Commented code is for old move system
+    public float speed = 5f, jumpSpeed = 8f;
+    public bool isGrounded;
+    public LayerMask layerMask;
+    public bool moveWhileJump = true;
+
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rigidBody;
     private Animator animator;
-
-    //Jumping Variables
-    public bool isJumping;
-    public float jumpSpeed = 8f;
-    private float rayCastLengthCheck = 0.005f;
-    private float width;
-    private float height;
-    public float jumpDurationThreshold = 0.05f;
-    private float jumpDuration;
-    public float airAccel = 1f;
+    private Transform playerTrans, groundTrans;
 
     //Declaraction of Variables for Mechanics
     public int playerHealth = 5;
 
-    public void Awake()
+    // Use this for initialization
+    public void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        animator = this.GetComponent<Animator>();
+        rigidBody = this.GetComponent<Rigidbody2D>();
+        playerTrans = this.GetComponent<Transform>();
 
-        width = GetComponent<Collider2D>().bounds.extents.x + 0.01f;
-        height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
+        groundTrans = GameObject.Find(this.name + "/tag_ground").transform;
     }
 
-    // Use this for initialization
-    public void Start ()
+    public void Move(float horizontalInput)
     {
-        playerHealth = 5;
-	}
+        if (!moveWhileJump && !isGrounded)
+            return;
+        Vector2 moveVelocity = rigidBody.velocity;
+        moveVelocity.x = horizontalInput * speed;
+        rigidBody.velocity = moveVelocity;
+    }
 
-    //Update the movement code of the player
+    public void Jump()
+    {
+        if (isGrounded)
+            rigidBody.velocity = jumpSpeed * Vector2.up;
+    }
+
+
     public void FixedUpdate()
     {
-        var acceleration = 0f;
-        var xVelocity = 0f;
+        isGrounded = Physics2D.Linecast(rigidBody.position, groundTrans.position, layerMask);
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
 
-        if (PlayerIsOnGround())
-        {
-            acceleration = accel;
-        }
-        else
-        {
-            acceleration = airAccel;
-        }
 
-        //Check input of Horizontal Movement
-        if (PlayerIsOnGround() && input.x == 0)
-        {
-            xVelocity = 0f;
-        }
-        else
-        {
-            xVelocity = rigidBody.velocity.x;
-        }
+        Move(input.x);
+        animator.SetFloat("Speed", (float)Mathf.Abs(input.x));
 
-        //Check input of Horizontal Movement
-        if (input.x ==0)
-        {
-            xVelocity = 0f;
-        }
-        else
-        {
-            xVelocity = rigidBody.velocity.x;
-        }
-
-        //move Player
-        rigidBody.AddForce(new Vector2(((input.x * speed) - rigidBody.velocity.x) * acceleration, 0));
-        rigidBody.velocity = new Vector2(xVelocity, rigidBody.velocity.y);
-
-        //jumping!
-        if (isJumping && jumpDuration < jumpDurationThreshold)
-        {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpSpeed);
-        }
-    }
-
-    //Jumping check Function - is the player able to jump?
-    public bool PlayerIsOnGround()
-    {
-        bool groundCheck1 = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - height), -Vector2.up, rayCastLengthCheck);
-        bool groundCheck2 = Physics2D.Raycast(new Vector2(transform.position.x + (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLengthCheck);
-        bool groundCheck3 = Physics2D.Raycast(new Vector2(transform.position.x - (width - 0.2f), transform.position.y - height), -Vector2.up, rayCastLengthCheck);
-
-        if (groundCheck1 || groundCheck2 || groundCheck3)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-
-    }
-
-    // Update is called once per frame
-    public void Update ()
-    {
-        //Receive player input
-        input.x = Input.GetAxis("Horizontal");
-        input.y = Input.GetAxis("Jump");
-
-        animator.SetFloat("Speed", Mathf.Abs(input.x));
-
-        //Handle Horizontal Movement
-        if (input.x > 0f)
-        {
+        if (input.x > 0)
             spriteRenderer.flipX = false;
-        }
-        else if (input.x < 0f)
-        {
-            spriteRenderer.flipX = true;            
-        }
+        else if (input.x < 0)
+            spriteRenderer.flipX = true;
 
-        //Jump mechanics
-        if (input.y >= 1f)
-        {
-            jumpDuration += Time.deltaTime;
-        }
-        else
-        {
-            isJumping = false;
-            jumpDuration = 0f;
-        }
-
-        //checks if the player is on the ground and able to jump
-        if (PlayerIsOnGround() && isJumping == false)
-        {
-            if (input.y > 0f)
-            {
-                isJumping = true;
-            }
-        }
-
-        if (jumpDuration > jumpDurationThreshold) input.y = 0f;
+        if (Input.GetButtonDown("Jump"))
+            Jump();
     }
 
     //When the player touches an interactable object (IE: Batteries, maybe, or Flashlight Upgrades
